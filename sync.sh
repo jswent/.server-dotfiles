8085b5c4ac0119e27953b795014566760e8ac318
jswent <94636565+jswent@users.noreply.github.com>
2022-05-21 13:03:41 -0500

    added secondary verification for oh-my-zsh install

--- sync.sh
+++ sync.sh
@@ -10,10 +10,24 @@ else
 fi
 
 # check if oh-my-zsh is installed
-if [ -d "~/.oh-my-zsh" ]
+if [ -d ~/.oh-my-zsh ]
 then 
-  echo "oh-my-zsh was found on this system, skipping install"
+  echo "oh-my-zsh was found on this system, skipping install..."
 else
-  echo "oh-my-zsh was not found on this system, skipping install..."
+  echo "oh-my-zsh was not found on this system, installing now..."
   curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh 
+
+  # verify installation was successful
+  if [ -d ~/.oh-my-zsh ]
+  then 
+    echo "oh-my-zsh was successfully installed"
+  else 
+    echo "oh-my-zsh failed to install, please re-run script"
+    exit 1
+  fi 
 fi
+
+# check required plugins are installed
+
+
+
